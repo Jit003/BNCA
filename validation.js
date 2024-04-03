05bf744fa83f2d7092d2ccc16acc3f586b6773b6
Jitendra <sahoojitendra1923@gmail.com>
2024-04-03 15:55:06 +0530

    validation for names has been done.

---
+++ validation.js
@@ -0,0 +1,26 @@
+function nameValidate(id) {
+    var regName = /^[a-zA-Z]+$/;
+    var msg = "";
+    
+    if (id=="firstName") {
+        msg = document.getElementById('fname-msg');
+    } else {
+        msg = document.getElementById('lname-msg');
+    }
+
+    id = document.getElementById(id);
+    
+    if (id.value == "" || id.value == undefined){
+        msg.style.display="none";
+    } else {
+        if(!regName.test(id.value)) {
+            msg.innerHTML="Invalid!";
+            msg.style.display="block";
+            msg.style.color="red";
+        } else {
+                msg.style.display="block";
+                msg.style.color="green";
+                msg.innerHTML="Valid!";
+        }
+    }
+}
